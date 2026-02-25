40131c069aaddabfec9de69a91d5d901747b554d
jonrgomes <jonrgomes@gmail.com>
2026-02-25 18:41:23 -0300

    Modulo_1

---
+++ blocos.py
@@ -0,0 +1,6 @@
+idade = 56
+
+if idade < 18:
+    print("Menor de idade")
+else:
+    print("Maior de idade!")
---
+++ sintaxe.py
@@ -0,0 +1,7 @@
+# Esse é meu primeiro comentário!
+"""Esse é meu primeiro comentário!
+Essa é a sedunda linha de comentário
+"""
+
+nome =  "Jonatas" + \
+"Gomes"
