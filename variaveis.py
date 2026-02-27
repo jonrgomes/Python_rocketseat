b9a7a833b6e26bb9a5486ed0c8971481342e9191
jonrgomes <jonrgomes@gmail.com>
2026-02-27 19:33:11 -0300

    Modulo_1

--- variaveis.py
+++ variaveis.py
@@ -1,3 +1,7 @@
-nome_completo = print("Jonatas Ribeiro Gomes")
-idade = print(25)
+#variavel_função_e_método
+nome_completo = "Jonatas Ribeiro Gomes"
+idade = 32
+
+#Classes
+nomeCompleto = "Jonatas Ribeiro Gomes"
 
