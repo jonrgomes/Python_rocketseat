554f67fd4d47168fdbe0e92396174d67c5941fba
jonrgomes <jonrgomes@gmail.com>
2026-02-27 19:31:44 -0300

    Modulo_1

---
+++ numeros.py
@@ -0,0 +1,44 @@
+#numeros_inteiros
+numero_inteiro = 4
+print("Interio =", numero_inteiro)
+
+#Real_com_número_flutuate
+numero_real = 3.14
+print("Real com ponto flutuante =", numero_real)
+
+#type()
+print("Tipo da variavel inteiro =", type(numero_inteiro))
+print("Tipo da variave real =", type(numero_real))
+
+#soma +
+soma = 1 + 1
+print ("1 + 1 =", soma)
+
+# subtracao - 
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# mutiplicacao *
+multiplicacao = 2 * 2
+print("2 x 2 =", multiplicacao)
+
+# divisão /
+divisao = 5 / 2
+print("5 / 2 =", divisao)
+print("Tipo de variavel do resultado da divisao =", type(divisao))
+
+# int()
+print("5 / 2 =", int(divisao))
+
+# float()
+print("Valor da soma em float = ", float(soma))
+
+# modulo %
+modulo = 5 % 2
+print("O modulo da divisão 5 / 2 =", modulo)
+
+# Divisão inteira //
+# divisão /
+divisao = 5 // 2
+print("5 // 2 =", divisao)
+print("Tipo de variavel do resultado da divisao =", type(divisao))
