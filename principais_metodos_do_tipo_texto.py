769bb8957f9b9c0ce6cfb03cc5b029799c3ffb06
jonrgomes <jonrgomes@gmail.com>
2026-02-28 21:18:50 -0300

    Modulo_1

---
+++ principais_metodos_do_tipo_texto.py
@@ -0,0 +1,14 @@
+# Variaveis são imultaveis
+
+nome = "Jonatas"
+sobrenome = "Gomes"
+nome_completo = "Jonatas Gomes"
+
+# Em Maisuculas
+print(nome.upper())
+
+# Em Minusculas
+print(nome_completo.lower())
+
+# Acessar letras da Variavel
+nome[0]
