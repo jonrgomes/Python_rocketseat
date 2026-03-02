2572ccd4f0e4500536939472cebab21d1dca0fec
jonrgomes <jonrgomes@gmail.com>
2026-03-02 19:11:27 -0300

    Modulo_1

--- principais_metodos_do_tipo_texto.py
+++ principais_metodos_do_tipo_texto.py
@@ -11,10 +11,19 @@ print(nome.upper())
 print(nome_completo.lower())
 
 # Acessar letras da Variavel
-nome[0]
+print(nome[0])
 
 # Contar ocorrencias dentro da str
 print(nome_completo.count("a"))
 
 # Posição de uma letra
 print(nome_completo.find("a"))
+
+# Codificação - Conversão de str para bits
+print(nome.encode())
+
+# Decodificar o que esta em bits
+print(nome.encode().decode())
+
+# Substitui uma str por outra str
+print(nome_completo.replace("a", "e"))
