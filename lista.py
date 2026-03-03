5ff8bf81d7ee78f1c63692c2ecf5e407dfb8d22f
jonrgomes <jonrgomes@gmail.com>
2026-03-03 20:11:22 -0300

    Modulo_1

---
+++ lista.py
@@ -0,0 +1,13 @@
+#  Lista é uma conleção de elementos ordenaveis e multaveis.
+# Declaração
+minha_lista = [1, 2, 3, 4, 5, "rocketseta", True, False]
+
+# Exibir a lista
+print("Minha lista de exemplo:", minha_lista)
+
+# Exibir de forma individual
+print("minha_lista[0] =", minha_lista[0])
+print("minha_lista[5] =", minha_lista[5])
+print("minha_lista[2:7] =", minha_lista[2:7])
+print("minha_lista[:6] =", minha_lista[:6])
+print("minha_lista[2:] =", minha_lista[2:])
