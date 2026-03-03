792b6f2a98e32fdfbe740dcf8e19ab5358c33f0e
jonrgomes <jonrgomes@gmail.com>
2026-03-03 20:01:07 -0300

    Modulo_1

---
+++ boleanos.py
@@ -0,0 +1,32 @@
+# Boleanos contemplam apenas 2 condições
+
+# Condição verdadeira
+True
+
+# Condição Falsa
+False
+
+# Condicionias
+# if = see condição
+# else = se não
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco ELSE não será executada")
+
+# Operadores logicos: and e or
+
+# AND
+if True and True:
+    print("Bloco será executado")
+
+if True and False:
+    print("Bloco não será executado")
+
+# OR
+if True or False:
+    print("Bloco OR vai ser executado")
+if False or False:
+    print("Bloco OR não vai ser executado")
+if True or True:
+    print("Bloco OR vai ser executado")
